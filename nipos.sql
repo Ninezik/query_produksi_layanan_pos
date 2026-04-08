8fa2bc076dcfec5b5ba2af3a9b2b210c8bb0171b
ninezik <30277780+Ninezik@users.noreply.github.com>
2026-04-08 21:14:50 +0700

    Update nipos.sql

--- nipos.sql
+++ nipos.sql
@@ -1,6 +1,7 @@
 SELECT
 date(nipos.connote__created_at ) AS tanggal,
 customer_code,nipos.nipos.location_data_created__custom_field__nopen ,
+nipos.nipos.connote__connote_service ,
 COUNT(connote__connote_code)produksi,
 SUM(connote__connote_service_price +connote__connote_surcharge_amount )pendapatan,
 'KURIR LOGISTIK' sumber
@@ -8,4 +9,5 @@ FROM nipos.nipos
 WHERE connote__created_at > '20260101'
 AND UPPER(connote__location_name) != 'AGP TESTING LOCATION'
 AND UPPER(connote__connote_state) NOT IN ('CANCEL','PENDING')
-group by 1,2,3
+and nipos.nipos.connote__connote_service !='LNINCOMING'
+group by 1,2,3,4
