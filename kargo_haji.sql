c0714f4a7142a732bceb96b607ca442d357d1a53
ninezik <30277780+Ninezik@users.noreply.github.com>
2026-04-08 23:26:53 +0700

    Create kargo_haji.sql

---
+++ kargo_haji.sql
@@ -0,0 +1,12 @@
+SELECT date(created_at)created_at ,
+kargo_haji_kolekting.reciver_zipcode ,
+'KARGO HAJI' as kategori,
+COUNT(distinct no_resi)produksi,
+SUM(total_fee_idr)/(1+(1.1/100)) pendapatan,
+SUM(total_weight)berat,
+'KURIR LOGISTIK' sumber
+FROM kargo.kargo_haji_kolekting
+where UPPER(status)='MANIFEST'
+and is_paid='t'
+GROUP BY
+1,2
